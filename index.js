90f707dc68766c3d431cdc2f3ba872e6b7d3a868
Hannes <h.geissler@schille.com>
2020-03-16 13:41:24 +0100

    serverside code

---
+++ index.js
@@ -0,0 +1,26 @@
+// -----------------------------------------------------------------------------
+// Configure Express
+// -----------------------------------------------------------------------------
+// setting up server
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io').listen(server)
+
+// how do we serve client-side files (e.g. public-folder)
+const path = require('path')
+app.use(express.static(path.join(__dirname, 'public')))
+// --> views-folder -> ejs files
+app.set('view engine', 'ejs')
+
+server.listen(process.env.PORT || 3000, () => console.log('Server listening'))
+
+// -----------------------------------------------------------------------------
+// Routes
+// -----------------------------------------------------------------------------
+// views/chat.ejs
+app.get("/", (req,res) => res.render("chat"))
+
+// -----------------------------------------------------------------------------
+// Configure Web Sockets
+// -----------------------------------------------------------------------------
